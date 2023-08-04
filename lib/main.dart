72f8e74e6cae8126561fba7c1647475705015be6
Waldener Junior <waldener.dev@gmail.com>
2023-08-04 15:58:26 -0300

    feat: init project

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import './questao.dart';
+import './resposta.dart';
+
+void main() => runApp(const PerguntaApp());
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;
+    });
+
+    print(_perguntaSelecionada);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Questao(perguntas[_perguntaSelecionada]),
+            Resposta('Resposta 1', _responder),
+            Resposta('Resposta 2', _responder),
+            Resposta('Resposta 3', _responder),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  const PerguntaApp({super.key});
+
+  @override
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+}
---
+++ lib/resposta.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+
+class Resposta extends StatelessWidget {
+  final String texto;
+  final void Function() onSelecao;
+
+  const Resposta(this.texto, this.onSelecao, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.black,
+            textStyle: const TextStyle(fontSize: 20)),
+        onPressed: onSelecao,
+        child: Text(texto),
+      ),
+    );
+  }
+}
